8f6ccc3251a93d5ab1074149841abd8ef23cea95
EmmyM_ighty <remi.emma04@gmail.com>
2020-12-24 22:47:42 +0100

    Add files via upload

---
+++ build/precache-manifest.df6b4a4772c5a9de51cb037eeace5957.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb11e10372d6fb81d24f227d491a181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddcdda487254e6e7dd83",
+    "url": "/static/css/main.934b7168.chunk.css"
+  },
+  {
+    "revision": "ad2987ec11121b947e67",
+    "url": "/static/js/2.14f92628.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.14f92628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcdda487254e6e7dd83",
+    "url": "/static/js/main.2a8a84c4.chunk.js"
+  },
+  {
+    "revision": "3d3368e6f1c7ed77ecd3",
+    "url": "/static/js/runtime-main.dc670065.js"
+  },
+  {
+    "revision": "2cf1493e68315d6b097294def1390902",
+    "url": "/static/media/togaj_logo.2cf1493e.png"
+  },
+  {
+    "revision": "8f901228fab809cc8213fb037efa1fa9",
+    "url": "/static/media/togaj_logo_edited.8f901228.png"
+  }
+]);
